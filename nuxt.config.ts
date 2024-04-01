d267bc2f838ade5193c67d1a8fb58ef71d4442e0
Sergey Krivtsov <flashgangsta@gmail.com>
2024-04-01 17:44:02 +0200

    Configue project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  srcDir: "src",
+  components: [
+    "~/components",
+    "~/../../shared/components"
+  ],
+  css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: "Arthapala",
+      htmlAttrs: {
+        lang: "en"
+      },
+    },
+  },
+  devServer: {
+    host: "0.0.0.0",
+    port: 3003
+  }
 })
