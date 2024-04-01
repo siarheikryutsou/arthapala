2fbff1fb9b33c9d053d37752891047390f816716
Sergey Krivtsov <flashgangsta@gmail.com>
2024-04-02 01:49:27 +0200

    Main page

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,57 @@
 <template>
-  <Header />
-  <main>
+  <header class="flex items-center justify-between md:justify-end p-8 bg-[#1B6ECF] md:bg-transparent">
+
+    <img class="md:hidden" src="/images/logo_mobile.svg" alt="Logo" />
+
+    <nav class="flex gap-6">
+      <a href="https://x.com/" target="_blank" aria-label="Read us on Twitter">
+        <nuxt-icon filled name="x" />
+      </a>
+      <a href="https://telegram.org/" target="_blank">
+        <nuxt-icon filled name="tg" />
+      </a>
+    </nav>
+  </header>
+
+  <main class="px-6">
     <NuxtPage />
   </main>
-  <Footer />
+
+  <footer class="py-6 px-6 md:px-20 text-white text-xs flex flex-col xl:flex-row xl:justify-between xl:items-end space-y-5 w-full">
+    <nav class="mx-auto md:mx-0">
+      <ul class="footer-contacts-list">
+        <li class="">
+          <a href="mailto:info@arthapala.vc" target="_blank">
+            <nuxt-icon filled name="mail" />
+            <span>info@arthapala.vc</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://maps.app.goo.gl/kdyzd4CcQ4ispkyy8" target="_blank" rel="noopener noreferrer">
+            <nuxt-icon filled name="geo" />
+            <span>PARALELNÍ POLIS, z.s., Dělnická 475/43, 170 00 Praha 7 Holešovice</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+    <nav>
+      <ul class="flex flex-col items-center gap-8 underline mt-16 md:mt-0 pb-0.5 text-sm md:text-xs md:flex-row md:items-start">
+        <li>
+          <nuxt-link to="/terms-and-conditions">Terms and Conditions</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/terms-and-conditions">General Risk Warning</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/terms-and-conditions">List of Prohibited Countries</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/terms-and-conditions">Impressum</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </footer>
 </template>
+<script setup lang="ts">
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,40 @@
 </script>
 
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="max-w-[790px] mx-auto flex flex-col items-center">
+    <img class="hidden md:block mt-24" src="/images/logo.svg" alt="Logo">
+    <h1 class="text-center text-white text-7xl font-bold mt-36 md:mt-6 max-w-[650px]">Custodial staking of Ethereum</h1>
+
+    <div class="mt-72 md:mt-16 flex flex-col xl:flex-row justify-between gap-10 xl:gap-16">
+
+      <div class="max-w-[375px] space-y-6 order-1 xl:order-none">
+        <p>Arthapala offers a custodial ETH staking as a service for companies and private customers.</p>
+        <p>No pooling of customer funds. High safety standards.</p>
+        <p>We operate according to European regulatory standards. All customers have to pass KYC/AML process during registration.</p>
+      </div>
+
+      <div>
+        <ul class="main-list">
+          <li>
+            <strong>5% fee</strong> of total earnings
+          </li>
+          <li>
+            <strong>32 ETH</strong> minimum delegation
+          </li>
+          <li>
+            monthly payouts
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <a class="mt-12 mb-14 py-2 px-8 rounded-full text-white text-lg font-extrabold border border-[#4D93E5] bg-gradient-to-b from-[#3E90F0] to-[#1B6ECF]"
+       href="#"
+       aria-label="">
+      Get started
+    </a>
   </div>
 </template>
+border-radius: 50px;
+border: 1px solid rgba(255, 255, 255, 0.40);
+background: linear-gradient(180deg, #3E90F0 0%, #1B6ECF 100%);
