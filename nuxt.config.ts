d38e5c6f2a07af29885cbb39d88fd61ad33751a4
Sergey Krivtsov <flashgangsta@gmail.com>
2024-04-01 21:21:04 +0200

    Install environment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   srcDir: "src",
   components: [
     "~/components",
     "~/../../shared/components"
   ],
+
   css: ["~/assets/css/main.css"],
+
   app: {
     head: {
       title: "Arthapala",
       htmlAttrs: {
         lang: "en"
-      },
-    },
+      }
+    }
   },
+
+  modules: [
+    "nuxt-icons",
+    ["@nuxtjs/google-fonts", {
+      families: {
+        Inter: [300, 400, 500, 700, 800]
+      }
+    }],
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+
   devServer: {
     host: "0.0.0.0",
     port: 3003
   }
-})
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app.vue",
+    "./src/components/**/*.vue",
+    "./src/pages/**/*.vue",
+    "./src/layouts/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
